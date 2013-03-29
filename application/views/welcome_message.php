9b665c4ec5af33d34f06bed619f492cce8e0a74f
zarzen <zarzen@ubuntu.(none)>
2013-03-28 18:45:33 -0700

    write the main page.now, contain the recommed-box and login-box

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,89 +5,67 @@
 	<title>Welcome to CodeIgniter</title>
 
 	<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
-
-
-	<style type="text/css">
-
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<link rel="stylesheet" type="text/css" href="./css/zz_css.css">
 </head>
 <body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	<div class="navbar">
+		<div class="navbar-inner">
+			<a href="#" class = "brand">ShareDoc</a>
+			<ul class = "nav">
+				<li class = "active"><a href="#">home</a></li>
+				<li><a href="#">xx1</a></li>
+			</ul>
+		</div>
+		
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div>
+	<div class="container">
+		<div class="alert">
+			<button type = "button" class = "close" data-dismiss = "alert">&times;</button>
+			<strong>这货只是个测试！</strong>
+		</div>
+
+		<div class="zz-line">
+			<div id = "zz-contain-recommend" class = "well">
+				<p>这里放推荐内容。</p>
+			</div>
 
 
+			<div id="zz-contain-login" >
+				<form class="form-horizontal">
+    				<div class="control-group">
+    					<label class="control-label" for="inputEmail">Email</label>
+    					<div class="controls">
+      						<input type="text" id="inputEmail" placeholder="Email">
+    					</div>
+  					</div>
+	  				<div class="control-group">
+					    <label class="control-label" for="inputPassword">Password</label>
+				    	<div class="controls">
+      						<input type="password" id="inputPassword" placeholder="Password">
+				    	</div>
+				    </div>
+  					<div class="control-group">
+    					<div class="controls">
+					    <label class="checkbox">
+				        	<input type="checkbox"> 记住我
+					    </label>
+			      			<button type="submit" class="btn">登陆</button>
+    					</div>
+  					</div>
+				</form>
+			</div>
+		</div>
+
+		<div class="zz-line">
+			<div id="zz-"></div>
+
+		</div>
+
+
+	</div>
+
+	
 	<script type="text/javascript" src="./js/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
 </body>
